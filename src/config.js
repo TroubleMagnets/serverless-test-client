c75d82f5a6fa9a721f07e59ceafff98b422638fe
matthewymwang@gmail.com <matthewwang@Matthews-MacBook-Pro.local>
2020-10-07 14:47:47 -0700

    setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-1dqhbqrqgot60"
+  },
+  apiGateway: {
+    REGION: "us-east-2,
+    URL: "https://dkc0rvzeuj.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_Q9q4ukI3T",
+    APP_CLIENT_ID: "11h111b7ind9s74n54hhdksqej",
+    IDENTITY_POOL_ID: "us-east-2:06442137-5bc3-4aff-9d14-a0293cd541d9"
+  }
+};
